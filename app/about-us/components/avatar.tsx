c2926c15bfae817a5ee69401324967ff637a61b3
Aaron Lee <aaronlab.net@gmail.com>
2024-08-18 15:54:45 +0900

    Not Found

--- app/about-us/components/avatar.tsx
+++
@@ -1,9 +0,0 @@
-export default function Avatar() {
-  return (
-    <img
-      src="https://avatars.githubusercontent.com/u/44143294?v=4"
-      alt="Avatar"
-      style={{ borderRadius: '50%', width: 100, height: 100 }}
-    />
-  )
-}
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,10 @@
+import Navigation from "./components/navigation";
+
 export default function NotFound() {
   return (
     <div>
+      <Navigation />
       <h1>404 - Page Not Found</h1>
     </div>
-  )
+  );
 }
